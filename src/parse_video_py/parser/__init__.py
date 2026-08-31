bdf20c74760a93b3e5303aa55b7b63319fedd698
hinspath <hinspath@gmail.com>
2026-08-31 18:10:27 +0800

    fix: support xhslink.cn share URLs

--- src/parse_video_py/parser/__init__.py
+++ src/parse_video_py/parser/__init__.py
@@ -131,6 +131,7 @@ video_source_info_mapping = {
         "domain_list": [
             "www.xiaohongshu.com",
             "xhslink.com",
+            "xhslink.cn",
         ],
         "parser": RedBook,
     },
